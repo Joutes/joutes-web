87a3bd99bded5480bd523e91f94ca0f62fbc675f
AshonsDev <ponceau.yoann@gmail.com>
2026-05-14 17:01:41 +0200

    Fix des warnings

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,4 @@
 import axios from 'axios';
-import { useAuthStore } from '@features/auth/store/auth.store';
 
 const api = axios.create({
     baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000',
@@ -7,10 +6,6 @@ const api = axios.create({
 
 // Intercepteur pour injecter le token
 api.interceptors.request.use((config) => {
-    const token = useAuthStore.getState().accessToken;
-    if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
-    }
     return config;
 });
 
